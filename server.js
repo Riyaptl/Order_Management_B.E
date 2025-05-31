8aefc6e8f0990a9361868e052e407b5492a2078a
Riya Patel <riyaptl3473@gmail.com>
2025-05-31 12:16:36 +0530

    fix: debugging with Allow-Control

--- server.js
+++ server.js
@@ -18,43 +18,48 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url} Origin: ${req.headers.origin}`);
+  next();
+});
+
 // CORS
 // app.use(cors({
 //   origin: 'http://localhost:3000',
 //   credentials: true,  // if you use cookies or auth headers
 // }));
 
-// const allowedOrigins = [
-//   "https://order-management-f-e-mk4n-ptmev3v3f.vercel.app",
-//   "http://localhost:3000",
-// ];
+const allowedOrigins = [
+  "https://order-management-f-e-mk4n-ptmev3v3f.vercel.app",
+  "http://localhost:3000",
+];
 
-// const allowCredentials = (req, res, next) => {
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//   }
-//   next();
-// };
+const allowCredentials = (req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  next();
+};
 
-// const corsOptions = {
-//   origin: function(origin, callback) {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true,
-//   optionsSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
-// app.use(allowCredentials);
-// app.use(cors(corsOptions));
+app.use(allowCredentials);
+app.use(cors(corsOptions));
 // app.options("/", cors(corsOptions));
 
-app.use(cors())
+// app.use(cors())
 
 // DB connection
 connection()
